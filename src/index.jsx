3c1065f3c6c5449aa9e88b694a599d7840c1bdde
Ashutosh Labh <145891404+ashubash@users.noreply.github.com>
2025-10-06 07:26:28 +0000

    modifying cpp-server, frontend simplistic design and deploy-server.yml

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/style.css';
+import FundusDemo from './FundusDemo';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <FundusDemo />
+  </React.StrictMode>
+);
